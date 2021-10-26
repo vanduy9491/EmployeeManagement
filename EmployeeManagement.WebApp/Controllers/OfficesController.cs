edf1d0724b22dcd170ddb9217ad82a3a9ce79f0b
tran van duy <vanduy9491@gmail.com>
2021-10-26 22:05:17 +0700

    commit

---
+++ EmployeeManagement.WebApp/Controllers/OfficesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EmployeeManagement.WebApp.DbContexts;
+using EmployeeManagement.WebApp.Entities;
+
+namespace EmployeeManagement.WebApp.Controllers
+{
+    public class OfficesController : Controller
+    {
+        private readonly AppDbContexts _context;
+
+        public OfficesController(AppDbContexts context)
+        {
+            _context = context;
+        }
+
+        // GET: Offices
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Offices.ToListAsync());
+        }
+
+        // GET: Offices/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var office = await _context.Offices
+                .FirstOrDefaultAsync(m => m.OfficeId == id);
+            if (office == null)
+            {
+                return NotFound();
+            }
+
+            return View(office);
+        }
+
+        // GET: Offices/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Offices/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Office office)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(office);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(office);
+        }
+
+        // GET: Offices/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var office = await _context.Offices.FindAsync(id);
+            if (office == null)
+            {
+                return NotFound();
+            }
+            return View(office);
+        }
+
+        // POST: Offices/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Office office)
+        {
+            if (id != office.OfficeId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(office);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OfficeExists(office.OfficeId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(office);
+        }
+
+        // GET: Offices/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var office = await _context.Offices
+                .FirstOrDefaultAsync(m => m.OfficeId == id);
+            if (office == null)
+            {
+                return NotFound();
+            }
+
+            return View(office);
+        }
+
+        // POST: Offices/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var office = await _context.Offices.FindAsync(id);
+            _context.Offices.Remove(office);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OfficeExists(int id)
+        {
+            return _context.Offices.Any(e => e.OfficeId == id);
+        }
+    }
+}
